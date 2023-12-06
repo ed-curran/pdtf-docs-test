839c2dcd3024e043fa56e6a270939f334a4c118c
Edward Curran <edward@jolocom.com>
2023-12-06 15:33:32 +0100

    don't ask

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,6 +1,7 @@
-import { API } from '@stoplight/elements';
 import * as PrismObject from 'prismjs'
+//if you remove this things will break
 globalThis.Prism = PrismObject
+import { API } from '@stoplight/elements';
 import {TryIt, withMosaicProvider, withStyles,  withRouter,
   withPersistenceBoundary,
   withQueryClientProvider,} from '@stoplight/elements-core';
