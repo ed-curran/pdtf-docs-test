66a0cb4f0bd36f9d9243ccc4552cffff4bd10b06
Edward Curran <edward@jolocom.com>
2023-12-18 21:21:41 +0100

    feat: add api and guide links to header

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -4,20 +4,28 @@ globalThis.Prism = PrismObject
 import { API } from '@stoplight/elements';
 import {TryIt, withMosaicProvider, withStyles} from '@stoplight/elements-core';
 import {JsonSchemaViewer} from '@stoplight/json-schema-viewer'
+import useBreakpoint from '../lib/useBreakpoints.ts'
 export function PdtfApi({ layout = 'sidebar', currentVersion = 'v1' }) {
+  const size = useBreakpoint();
+  //hacks to hide spinner once loaded
+  //astro doesn't seem to have a nicer way to display a spinner / do suspense stuff
+  //@ts-ignore
+  document.querySelector(".spinner").style.display = "none"
+
    return  (
         <API
             apiDescriptionUrl="/pdtf-api-1.2.0.yaml" //this works because this file is is in our public dir. importing directly is awkward cus this component runs client side only
             apiDescriptionDocument={''}
             router='hash'
-            className={'not-content'}
+            layout={size === 'sm' ? 'stacked' : 'sidebar'}
+            className='not-content'
+            class=''
         />
     );
 }
 
 const TryItWithProvider = withMosaicProvider(withStyles(TryIt))
 export function PdtfTryIt({ layout = 'sidebar', currentVersion = 'v1' }) {
-
     return  (
       // <SSRProvider>p
       //   <MosaicProvider>
